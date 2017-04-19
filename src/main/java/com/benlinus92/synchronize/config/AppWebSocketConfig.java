f6fef28b129a3da7bc13365c5a2ac370500e3d30
father-gregor <benlinus92@gmail.com>
2017-04-20 00:31:13 +0300

    Change interceptor

--- src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
+++ src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import com.benlinus92.synchronize.interceptor.SessionAttributeHandshakeInterceptor;
 
@@ -20,7 +21,7 @@ public class AppWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
 	}
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/videoroom").withSockJS().setInterceptors(sessionAttributeHandshakeInterceptor());
+		registry.addEndpoint("/videoroom").withSockJS().setInterceptors(new HttpSessionHandshakeInterceptor());
 	}
 	@Bean
 	public SessionAttributeHandshakeInterceptor sessionAttributeHandshakeInterceptor() {
