c1cde62603f86da2c3e4906675c11ec5025a0e38
father-gregor <benlinus92@gmail.com>
2017-04-19 23:54:07 +0300

    Add interceptor to set WebSocket Session Attribute

--- src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
+++ src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
@@ -1,10 +1,13 @@
 package com.benlinus92.synchronize.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import com.benlinus92.synchronize.interceptor.SessionAttributeHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,7 +20,11 @@ public class AppWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
 	}
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/videoroom").withSockJS();
+		registry.addEndpoint("/videoroom").withSockJS().setInterceptors(sessionAttributeHandshakeInterceptor());
 	}
-
+	@Bean
+	public SessionAttributeHandshakeInterceptor sessionAttributeHandshakeInterceptor() {
+		return new SessionAttributeHandshakeInterceptor();
+	}
+	
 }
