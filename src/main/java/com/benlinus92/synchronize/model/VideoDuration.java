a8dfe2d5348fca30e445b47441c5034561cb6cdf
father-gregor <benlinus92@gmail.com>
2017-04-26 18:15:33 +0300

    Add class for duration of video

---
+++ src/main/java/com/benlinus92/synchronize/model/VideoDuration.java
@@ -0,0 +1,20 @@
+package com.benlinus92.synchronize.model;
+
+public class VideoDuration {
+	private String videoId;
+	private double duration;
+	
+	public String getVideoId() {
+		return videoId;
+	}
+	public void setVideoId(String videoId) {
+		this.videoId = videoId;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	
+}
