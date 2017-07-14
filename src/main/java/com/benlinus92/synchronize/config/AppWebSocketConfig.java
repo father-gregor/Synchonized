629de377a9ab06af641e36dfe9cf17d71ff79cab
father-gregor <benlinus92@gmail.com>
2017-07-14 21:46:45 +0300

    Test heartbeat ping scheduler

--- src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
+++ src/main/java/com/benlinus92/synchronize/config/AppWebSocketConfig.java
@@ -3,6 +3,7 @@ package com.benlinus92.synchronize.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,12 +17,14 @@ public class AppWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic", "/queue");
+		ThreadPoolTaskScheduler pingerScheduler = new ThreadPoolTaskScheduler();
+		pingerScheduler.initialize();
+		config.enableSimpleBroker("/topic", "/queue").setHeartbeatValue(new long[] {10000, 0}).setTaskScheduler(pingerScheduler);
 		config.setApplicationDestinationPrefixes("/app");
 	}
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/videoroom").withSockJS().setInterceptors(new HttpSessionHandshakeInterceptor()).setHeartbeatTime(10000);
+		registry.addEndpoint("/videoroom").withSockJS().setInterceptors(new HttpSessionHandshakeInterceptor());
 	}
 	@Bean
 	public SessionAttributeHandshakeInterceptor sessionAttributeHandshakeInterceptor() {
